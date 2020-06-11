d8c427b141992addd52c9e2b2b46150f8738e425
JuanBiantong <jbiantong@gmail.com>
2020-06-11 15:15:38 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+//Nomor 1
+/*let jobDesc = 'webdev';
+
+if(jobDesc=='hr'){
+    console.log('Tugas saya melakukan rekrutment untuk pegawai baru.')
+} else if(jobDesc=='webdev'){
+    console.log('Tugas saya melakukan pengembangan website.')
+}else {
+    console.log('Saya masih training.')
+}*/
+
+//Nomor 2
+/*let num1 = 5;
+let num2 = 5;
+if(num1>num2){
+    console.log('Angka pertama lebih besar dari angka kedua');
+}else if(num1<num2){
+    console.log('Angka pertama lebih kecil dari angka kedua');
+}else{
+    console.log('Angka pertama sama dengan angka kedua');
+}*/
+
+//Nomor 3
+/*let day = '7'
+switch(day){
+    case '1':
+        console.log('Hari Minggu');
+        break;
+    case '2':
+        console.log('Hari Senin');
+        break;
+    case '3':
+        console.log('Hari Selasa');
+        break;
+    case '4':
+        console.log('Hari Rabu');
+        break;
+    case '1':
+        console.log('Hari Minggu');
+        break;
+    case '5':
+        console.log('Hari Kamis');
+        break;
+    case '6':
+        console.log('Hari Jumat');
+        break;
+    case '7':
+        console.log('Hari Sabtu');
+        break;
+}*/
+
+//Nomor 4
+let direction = 'BOTTOM'
+switch(direction){
+    case 'UP':
+        console.log('Karakter berjalan ke atas');
+        break;
+    case 'RIGHT':
+        console.log('Karakter berjalan ke kanan');
+        break;
+    case 'BOTTOM':
+        console.log('Karakter berjalan ke bawah');
+        break;
+    case 'LEFT':
+        console.log('Karakter berjalan ke kiri');
+        break;
+}
